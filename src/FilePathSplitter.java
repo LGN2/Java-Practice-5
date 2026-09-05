9702cc3ba66bf8f802296bd202f72fe3ad1a84f4
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-05 18:04:20 +0400

    feat (src):  FilePathSplitter added main method

--- src/FilePathSplitter.java
+++ src/FilePathSplitter.java
@@ -1,2 +1,63 @@
+import java.util.Scanner;
+
 public class FilePathSplitter {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String originalPath;
+
+        IO.print("Enter full file path: ");
+        originalPath = sc.nextLine().trim();
+
+        if (originalPath.isBlank()) {
+
+            IO.println("Invalid path.");
+            sc.close();
+            return;
+        }
+
+        boolean hasBackslash = originalPath.contains("\\");
+
+        String normalizedPath = originalPath.replace('\\', '/');
+
+        int slashIndex = normalizedPath.lastIndexOf('/');
+
+        String folder;
+        String fileName;
+
+        if (slashIndex != -1) {
+
+            folder = normalizedPath.substring(0, slashIndex);
+            fileName = normalizedPath.substring(slashIndex + 1);
+
+        } else {
+
+            folder = "none";
+            fileName = normalizedPath;
+        }
+
+        String extension = getExtension(fileName);
+
+        String pathStyle;
+
+        if (hasBackslash) {
+
+            pathStyle = "Windows Path";
+
+        } else {
+
+            pathStyle = "Unix Path";
+        }
+
+        IO.println("\n========== PATH INFORMATION ==========");
+        IO.println("Original Path: " + originalPath);
+        IO.println("Folder: " + folder);
+        IO.println("File Name: " + fileName);
+        IO.println("Extension: " + extension);
+        IO.println("Path Style: " + pathStyle);
+
+        sc.close();
+    }
 }
