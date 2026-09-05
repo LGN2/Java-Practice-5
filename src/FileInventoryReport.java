6fbaca00d369483881435002922f59369f549620
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-05 18:23:23 +0400

    feat (src): FileInventoryReport added getExtension method

--- src/FileInventoryReport.java
+++ src/FileInventoryReport.java
@@ -116,4 +116,18 @@ public class FileInventoryReport {
                 largestFile + " (" + largestSize + " KB)"
         );
     }
+    public static String getExtension(String fileName) {
+
+        int dotIndex = fileName.lastIndexOf('.');
+
+        if (dotIndex == -1
+                || dotIndex == fileName.length() - 1) {
+
+            return "none";
+        }
+
+        return fileName
+                .substring(dotIndex + 1)
+                .toLowerCase();
+    }
 }
