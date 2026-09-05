fc65d0f97d1ed99c537a527bdab52715f00a561b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-05 17:55:35 +0400

    feat (src): FileNameAnalyzer  added main method

--- src/FileNameAnalyzer.java
+++ src/FileNameAnalyzer.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class FileNameAnalyzer {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String fileName;
+
+        IO.print("Enter file name: ");
+        fileName = sc.nextLine().trim();
+
+        int dotIndex = fileName.lastIndexOf('.');
+
+        if (fileName.isBlank()
+                || dotIndex == -1
+                || dotIndex == 0
+                || dotIndex == fileName.length() - 1) {
+
+            IO.println("Invalid file name.");
+            sc.close();
+            return;
+        }
+
+        String baseName = fileName.substring(0, dotIndex);
+        String extension = fileName.substring(dotIndex + 1).toLowerCase();
+
+        String type = classifyFile(extension);
+
+        IO.println("\n========== FILE INFORMATION ==========");
+        IO.println("File Name: " + fileName);
+        IO.println("Base Name: " + baseName);
+        IO.println("Extension: " + extension);
+        IO.println("Type: " + type);
+
+        sc.close();
+    }
 }
