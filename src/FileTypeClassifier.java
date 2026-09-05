322eacf8ac5924ed2905e0fbde6a0e4803c0dfdd
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-05 17:58:32 +0400

    feat (src): FileTypeClassifier  added main

--- src/FileTypeClassifier.java
+++ src/FileTypeClassifier.java
@@ -1,2 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileTypeClassifier {
+
+    public static void main(String[] args) {
+
+        List<String> fileNames = new ArrayList<>();
+
+        fileNames.add("report.pdf");
+        fileNames.add("notes.txt");
+        fileNames.add("photo.jpg");
+        fileNames.add("logo.png");
+        fileNames.add("Main.java");
+        fileNames.add("script.py");
+        fileNames.add("website.js");
+        fileNames.add("README");
+        fileNames.add("music.mp3");
+
+        int documentCount = 0;
+        int imageCount = 0;
+        int codeCount = 0;
+        int otherCount = 0;
+
+        IO.println("========== FILE TYPE CLASSIFIER ==========");
+
+        for (String fileName : fileNames) {
+
+            String extension = getExtension(fileName);
+            String type = classifyFile(extension);
+
+            IO.println(fileName + " -> " + type);
+
+            if (type.equals("Document")) {
+
+                documentCount++;
+
+            } else if (type.equals("Image")) {
+
+                imageCount++;
+
+            } else if (type.equals("Code")) {
+
+                codeCount++;
+
+            } else {
+
+                otherCount++;
+            }
+        }
+
+        IO.println("\n========== SUMMARY ==========");
+        IO.println("Total Files: " + fileNames.size());
+        IO.println("Documents: " + documentCount);
+        IO.println("Images: " + imageCount);
+        IO.println("Code Files: " + codeCount);
+        IO.println("Other Files: " + otherCount);
+    }
 }
